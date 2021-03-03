31b32608eac6a1cc8fd6be8627753bafd17510bf
Jose09011712 <jlopezt11@miumg.edu.gt>
2021-03-03 10:24:11 -0600

    Estructura

---
+++ Parcial/Prototipo1PDLL/CapaVistaParcial/frmLinea.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaVistaParcial
+{
+    public partial class frmLinea : Form
+    {
+        public frmLinea()
+        {
+            InitializeComponent();
+        }
+    }
+}
